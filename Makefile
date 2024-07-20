05c398d9e6893335578b4be367e8ae969b5fc8ae
Cale McEvoy <mca@int21h.xyz>
2024-07-19 21:31:53 -0300

    add fbink
    nothing is final in main.c please ignore it forever

--- Makefile
+++ Makefile
@@ -1,15 +1,22 @@
 CC = cc
-CFLAGS = -O0 -std=c99 -pedantic -g
+CFLAGS = -O0 -std=c99 -pedantic -g -IFBInk
+LDFLAGS = -L./FBInk/Release -lfbink
+
+OBJ = main.o vt100.o
 
 %.o: %.c
 	$(CC) -c -o $@ $(CFLAGS) $<
 
 all: inkterm
 
-inkterm: main.o vt100.o
-	$(CC) -o $@ $(CFLAGS) $^
+inkterm: FBInk/Release/libfbink.so $(OBJ)
+	$(CC) -o $@ $(CFLAGS) $(OBJ) $(LDFLAGS) 
+
+FBInk/Release/libfbink.so:
+	$(MAKE) -C FBInk LINUX=1 MINIMAL=1 BITMAP=1 DRAW=1 FONTS=1 shared 
 
 .PHONY: clean
 
 clean:
 	rm -f main.o vt100.o inkterm
+	$(MAKE) -C FBInk clean
--- main.c
+++ main.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+#include <fbink.h>
 
 #include "vt100.h"
+#include "x.h"
 
 int
 main(int argc, char *argv[])
 {
 	char *args[] = {"/bin/sh", "-c", "echo \"Hello, world!\"; echo \"$$\"; sleep 1", NULL};
-	vt100_init(4, 4, "/bin/sh", args);
+
+	int fb = fbink_open();
+	if (!fb)
+		die("fbink_open failed: %s\n", strerror(errno));
+
+	int r;
+	FBInkConfig fbc = {
+		.is_verbose = 1,
+		.fontname = SCIENTIFICA,
+	};
+	FBInkState s;
+	r = fbink_init(fb, &fbc);
+	fbink_cls(fb, &fbc, NULL, 0);
+	fbink_get_state(&fbc, &s);
+
+	vt100_init(s.max_rows, s.max_cols, "/bin/sh", args);
 
 	// TODO: The rest of the owl.
 	// For now, just shuffle between the pty and stdout
@@ -22,16 +41,19 @@ main(int argc, char *argv[])
 		// Write out contents of screen
 		for (int y = 0; y < term.rows; ++y) {
 			for (int x = 0; x < term.cols; ++x) {
-				char c = term.cells[(y*term.cols)+x].c;
-				if (c) putchar(c);
-				else putchar(' ');
+				fbc.row = y;
+				fbc.col = x;
+
+				fbc.is_inverted = (x == term.col && y == term.row);
+
+				char c[] = {term.cells[(y*term.cols)+x].c, 0};
+				if (c[0] && c[0] != ' ') fbink_print(fb, c, &fbc);
+				else if (fbc.is_inverted) fbink_print(fb, " ", &fbc);
 			}
-			putchar('|');
-			putchar('\n');
 		}
-		puts("$-$");
 		printf("%d %d\n", term.row, term.col);
 	}
 
+	fbink_close(fb);
 	vt100_free();
 }
