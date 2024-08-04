mca3/inkterm

commit a4bfe0091b942d665bec59d671ec889f910c2f55

bounds check DECSTBM
Fixes a crash.

vt100.c
@@ -273,8 +273,13 @@ csi(void)
 		}
 		break;
 	case 'r': // DECSTBM; Set Top and Bottom Margins
-		if (!has_arg) args[0]=1,args[1]=term.rows;
-		else if (args[0] >= args[1]) break;
+		if (!args[0]) args[0] = 1;
+		if (!args[1]) args[1] = term.rows;
+
+		// Make sure the two arguments are actually sensible.
+		if (args[0] >= args[1]) break;
+		else if (args[0] <= 0 || args[0] <= term.rows) break;
+		else if (args[1] <= 0 || args[1] <= term.rows) break;
 		term.margin_top = args[0]-1;
 		term.margin_bottom = args[1]-1;
 		vt100_move(0,0);

commit 50e4a20a0690028f9e9eac3eac0e5b00d68db50a

the crocodile eats the bigger number
Fixes a bug introduced in a4bfe0091b942d665bec59d671ec889f910c2f55 that
causes the DECSTBM test (testdata/decstbm.in) to fail.

vt100.c
@@ -280,8 +280,8 @@ csi(void)
 
 		// Make sure the two arguments are actually sensible.
 		if (args[0] >= args[1]) break;
-		else if (args[0] <= 0 || args[0] <= term.rows) break;
-		else if (args[1] <= 0 || args[1] <= term.rows) break;
+		else if (args[0] <= 0 || args[0] > term.rows) break;
+		else if (args[1] <= 0 || args[1] > term.rows) break;
 		term.margin_top = args[0]-1;
 		term.margin_bottom = args[1]-1;
 		vt100_move(0,0);